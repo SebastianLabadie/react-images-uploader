881f79e592d1473398a810eab57123be3fe3eb73
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-24 10:05:51 -0300

    dropzone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-import UploadImage from './components/UploadImage/UploadImage';
+import React, { useState } from "react";
+import "./App.css";
+import DropZone from "./components/DropZone/DropZone";
+import image from "./images/image.svg";
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isUploadSuccessful, setIsUploadSuccessful] = useState(false);
+  const [preview, setPreview] = useState(null);
+
+  // Handling file selection from input
+  const onFileSelected = (e) => {
+    if (e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = (values) => {
+    setSelectedFile(values.file);
+    // createFormData();
+  };
+  
+  const onDrop = (acceptedFile) => {
+    console.log(acceptedFile)
+  };
+
   return (
-    <Router>
     <div className="app">
-       <Switch>
-         <Route exact path="/" component={UploadImage}/>
-       </Switch>
+      <div className="card">
+        <div className="container">
+          <div className="container-title">
+              <h3 className="title">Upload your image</h3>
+          </div>
+          <div className="container-dropzone">
+            <h4 className="subtitle">File should be Jpeg, Png,...</h4>
+            <form onSubmit={handleSubmit}>
+              <DropZone image={image} onDrop={onDrop} preview={preview} />
+              <div>
+                <h4 className="text-or">Or</h4>
+                <div className="container">
+                  {isUploadSuccessful ? null : (
+                    <label htmlFor="file-picker" className="btn-file-picker">
+                      <input
+                        id="file-picker"
+                        type="file"
+                        style={{ display: "none" }}
+                        onChange={onFileSelected}
+                      />
+                      Choose a file
+                    </label>
+                  )}
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-    </Router>
   );
 }
 
--- frontend/src/components/DropZone/DropZone.js
+++ frontend/src/components/DropZone/DropZone.js
@@ -1,19 +1,23 @@
 import React from "react";
-import './DropZone.css'
+import "./DropZone.css";
 import { useDropzone } from "react-dropzone";
 
 function DropZone({ isError, preview, image, onDrop }) {
-  const { getRootProps, getInputProps, isDragReject } = useDropzone({
+  const {
+    getRootProps,
+    getInputProps,
+    isDragReject,
+    isDragActive,
+    isDragAccept,
+  } = useDropzone({
     accept: "image/*",
     onDrop: (acceptedFile) => {
       onDrop(acceptedFile[0]);
     },
   });
 
-  console.log(getInputProps);
-
   return (
-    <div {...getRootProps({ className: "dropzone" })}>
+    <div {...getRootProps({ className: isDragActive ? "dropzone active" : "dropzone" })}>
       <img className="image" src={image} alt="dropzone" />
 
       {!preview && (
